5e216a6708ec3aea39c4aaeb1288bf62e77a21c8
johnbuonassisi <johnbuonassisi@gmail.com>
2016-11-02 00:36:05 -0700

    - Added privacy strings in info.plist so the app can use camera and photo library - Added camera button and image picker to Challenge View Controller - Added segmented control functionality to Challenge View Controller - Created and configured the cell for Challenge View Controller

--- YourTake/View Controllers/ChallengeViewController.swift
+++ YourTake/View Controllers/ChallengeViewController.swift
@@ -10,25 +10,103 @@ import UIKit
 
 class ChallengeViewController: UIViewController,
                                UITableViewDelegate,
-                               UITableViewDataSource{
+                               UITableViewDataSource,
+                               UINavigationControllerDelegate,
+                               UIImagePickerControllerDelegate{
 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
+        super.viewDidLoad();
+        
+        // Load and register cell NIB
+        let nib : UINib = UINib(nibName: "ChallengeCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "ChallengeCellTest")
+        
+        // Set the cell height
+        tableView.rowHeight = ChallengeCell.CellRowHeight()
+        
         tableView.dataSource = self
         tableView.delegate = self
+        
+        // Setup the navigation bar
+        navigationItem.title = "Challenges"
+        
+        let rbbi = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.camera,
+                                   target: self,
+                                   action: #selector(ChallengeViewController.newChallenge))
+        navigationItem.rightBarButtonItem = rbbi
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 1
+        switch(segmentedControl.selectedSegmentIndex){
+        
+        case 0: // User Challenges
+            return UserDatabase.global.John().challenges!.count
+            
+        case 1: // Friend Challenges
+            return 0
+            
+        default: // ??
+            return 0
+            
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let myCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "myCell")
-        myCell.textLabel?.text = "Hello World!"
-        return myCell
+        switch(segmentedControl.selectedSegmentIndex){
+        
+        case 0: // User Challenges
+            let cell : ChallengeCell = tableView.dequeueReusableCell(withIdentifier: "ChallengeCellTest",
+                                                                     for: indexPath) as! ChallengeCell
+            cell.challengeImage.image = UserDatabase.global.John().challenges![indexPath.row].image
+            cell.name.text = UserDatabase.global.John().name
+            return cell
+            
+        case 1: // Friend Challenges
+            let cell : ChallengeCell = tableView.dequeueReusableCell(withIdentifier: "ChallengeCellTest",
+                                                                     for: indexPath) as! ChallengeCell
+            cell.challengeImage.image = UserDatabase.global.John().challenges![0].image
+            cell.name.text = UserDatabase.global.John().name
+            return cell
+            
+        default: // ??
+            let cell : ChallengeCell = tableView.dequeueReusableCell(withIdentifier: "ChallengeCellTest",
+                                                                     for: indexPath) as! ChallengeCell
+            cell.challengeImage.image = UserDatabase.global.John().challenges![0].image
+            cell.name.text = UserDatabase.global.John().name
+            return cell
+        }
+    }
+    
+    @IBAction func segmentChanges(_ sender: UISegmentedControl)
+    {
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool
+    {
+        return false
+    }
+    
+    func newChallenge()
+    {
+        let ip : UIImagePickerController = UIImagePickerController()
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera))
+        {
+            ip.sourceType = UIImagePickerControllerSourceType.camera
+        }
+        else
+        {
+            ip.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        }
+        
+        ip.delegate = self
+        self.present(ip, animated: true, completion: nil)
+        
     }
 }
