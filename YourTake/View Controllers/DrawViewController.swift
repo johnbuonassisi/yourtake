4687424050138d969b6d2b041466a48c343c0a9b
johnbuonassisi <johnbuonassisi@gmail.com>
2016-11-15 01:43:54 -0800

    - Significantly improved performance of freehand drawing when user draws a long path 	- Put a limit on the number of points in the path being drawn 	- When limit is reached, a new context is drawn - Updated drawing undo so that it works with the limited path size - Implemented action methods for the colour and stroke size sliders

--- YourTake/View Controllers/DrawViewController.swift
+++ YourTake/View Controllers/DrawViewController.swift
@@ -11,15 +11,74 @@ import UIKit
 class DrawViewController: UIViewController,
                           UINavigationControllerDelegate {
 
+    // MARK: Outlets
+    
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var drawingView: DrawPathsView!
     @IBOutlet weak var colourSlider: UISlider!
-    @IBOutlet weak var colourDisplay: UIImageView!
     @IBOutlet weak var brushSlider: UISlider!
-    @IBOutlet weak var brushDisplay: UIImageView!
     @IBOutlet weak var undoButton: UIButton!
+    
+    // MARK: Initializers
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        let rbbi = UIBarButtonItem(title: "Submit",
+                                   style: UIBarButtonItemStyle.plain,
+                                   target: self,
+                                   action: nil)
+        navigationItem.rightBarButtonItem = rbbi
+        navigationItem.title = "Draw"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let rbbi = UIBarButtonItem(title: "Submit",
+                                  style: UIBarButtonItemStyle.plain,
+                                  target: self,
+                                  action: nil)
+        navigationItem.rightBarButtonItem = rbbi
+        navigationItem.title = "Draw"
+        
+    }
+    
+    // MARK: Action Methods
     
     @IBAction func undoButtonPressed(_ sender: UIButton) {
         drawingView.undo()
+    }
+    
+    @IBAction func colourSliderValueChanged(_ sender: UISlider) {
+        
+        switch sender.value {
+        case 0..<20:
+            drawingView.currentStrokeColour = UIColor.yellow
+            setColourSliderTrackTint(withColour: UIColor.yellow)
+        case 20..<40:
+            drawingView.currentStrokeColour = UIColor.red
+            setColourSliderTrackTint(withColour: UIColor.red)
+        case 40..<60:
+            drawingView.currentStrokeColour = UIColor.blue
+            setColourSliderTrackTint(withColour: UIColor.blue)
+        case 60..<80:
+            drawingView.currentStrokeColour = UIColor.green
+            setColourSliderTrackTint(withColour: UIColor.green)
+        default:
+            drawingView.currentStrokeColour = UIColor.lightGray
+            setColourSliderTrackTint(withColour: UIColor.lightGray)
+        }
+    }
+    
+    @IBAction func strokeSizeSliderValueChanged(_ sender: UISlider) {
+        drawingView.currentStrokeSize = CGFloat(sender.value)
+    }
+    
+    // MARK: Private Custom Methods
+    
+    private func setColourSliderTrackTint(withColour colour:UIColor) {
+        colourSlider.minimumTrackTintColor = colour
+        colourSlider.maximumTrackTintColor = colour
     }
 }
--- YourTake/Views/DrawPathsView.swift
+++ YourTake/Views/DrawPathsView.swift
@@ -10,14 +10,33 @@ import UIKit
 
 struct Path {
     var points: [CGPoint]
+    var colour: UIColor
+    var strokeSize : CGFloat
 }
 
 class DrawPathsView: UIView {
     
+    // MARK: Private Constant Member Variables
+    private let maxPointsInCurrentPath : Int = 50
+    
+    // MARK: Private Mutable Member Variables
+    
     private var completedPaths: [Path] = [Path]()
     private var currentPath: Path?
     private var incrementalImage : UIImage?
+    
+    // MARK: Internal Member Variables
+    
     var backgroundImage: UIImage?
+    var currentStrokeColour: UIColor = UIColor.blue
+    var currentStrokeSize: CGFloat = 8.0
+    
+    // MARK: Initializers
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        isMultipleTouchEnabled = false
+    }
     
     // MARK: Custom Drawing
     
@@ -26,6 +45,7 @@ class DrawPathsView: UIView {
         incrementalImage?.draw(in: rect)
         
         if let currentPath = currentPath {
+            currentPath.colour.setStroke()
             stroke(withPath: currentPath)
         }
         
@@ -38,9 +58,10 @@ class DrawPathsView: UIView {
         let touch = touches.first
         let touchLocation = touch!.location(in: self)
         var currentPathPoints = [CGPoint]()
-        currentPathPoints.reserveCapacity(1000)
         currentPathPoints.append(touchLocation)
-        currentPath = Path(points: currentPathPoints)
+        currentPath = Path(points: currentPathPoints,
+                           colour: currentStrokeColour,
+                           strokeSize: currentStrokeSize)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -48,6 +69,14 @@ class DrawPathsView: UIView {
         let touch = touches.first
         let touchLocation = touch!.location(in: self)
         currentPath!.points.append(touchLocation)
+        
+        if(currentPath!.points.count > maxPointsInCurrentPath)
+        {
+            touchesEnded(touches, with: event)
+            touchesBegan(touches, with: event)
+            return
+        }
+        
         setNeedsDisplay()
     }
     
@@ -60,41 +89,85 @@ class DrawPathsView: UIView {
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        touchesEnded(touches, with: event)
     }
     
     // MARK: Internal Custom Methods
     
     func setBackground(withImage image: UIImage?) {
+        
         backgroundImage = image
+        
         drawBitmap()
         setNeedsDisplay()
     }
     
-    func undo()
-    {
-        if completedPaths.count > 0 {
+    func undo1() {
+        
+        if(completedPaths.count > 0) {
+            
             completedPaths.removeLast()
+            
             UIGraphicsBeginImageContextWithOptions(self.bounds.size, true, 0.0)
             backgroundImage?.draw(at: CGPoint.zero)
             backgroundImage?.draw(in: self.bounds)
+            
             for path in completedPaths {
                 stroke(withPath: path)
             }
+            
             incrementalImage = UIGraphicsGetImageFromCurrentImageContext()
             UIGraphicsEndImageContext()
             setNeedsDisplay()
         }
+    }
+    
+    func undo() {
         
+        if(completedPaths.count > 0) {
+            
+            // Remove completed paths if they are connected to eachother
+            
+            var firstPointLastPath: CGPoint? // first point in last connected path
+            var lastPointNextPath: CGPoint? // last point in the next connected path
+            repeat {
+                firstPointLastPath = completedPaths.removeLast().points[0]
+                if completedPaths.count == 0 {
+                    break
+                }
+                lastPointNextPath = completedPaths.last!.points.last
+            } while firstPointLastPath == lastPointNextPath
+        }
+        
+        // Create the new context using the saved background image and the remaining paths
+        
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, true, 0.0)
+        backgroundImage?.draw(at: CGPoint.zero)
+        backgroundImage?.draw(in: self.bounds)
+        
+        for path in completedPaths {
+            stroke(withPath: path)
+        }
+        
+        incrementalImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        setNeedsDisplay()
     }
     
     // MARK: Private Custom Methods
     
-    private func stroke(withPath path: Path?){
+    private func stroke(withPath path: Path?) {
         
         if let path = path {
+            
+            if path.points.count < 2 {
+                return
+            }
+            
             let bp : UIBezierPath = UIBezierPath()
-            bp.lineWidth = 10
+            bp.lineWidth = path.strokeSize
             bp.lineCapStyle = CGLineCap.round
+            path.colour.setStroke()
         
             for index in 0...path.points.count - 2 {
                 bp.move(to: path.points[index])
@@ -107,16 +180,13 @@ class DrawPathsView: UIView {
     private func drawBitmap() {
         
         UIGraphicsBeginImageContextWithOptions(self.bounds.size, true, 0.0)
-        UIColor.black.setStroke()
         
         if incrementalImage == nil {
             if backgroundImage == nil {
                 let rectPath = UIBezierPath(rect: self.bounds)
                 UIColor.white.setFill()
                 rectPath.fill()
-            }
-            else
-            {
+            }else {
                 incrementalImage = backgroundImage
             }
         }
