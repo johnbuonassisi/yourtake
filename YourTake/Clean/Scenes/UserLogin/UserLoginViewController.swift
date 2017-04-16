71ef9f3b0accf7626e1f8b594ecb4810a57fcc3b
johnbuonassisi <johnbuonassisi@gmail.com>
2017-04-15 23:45:04 -0700

    Added clean version of forgot login password scene

--- YourTake/Clean/Scenes/UserLogin/UserLoginViewController.swift
+++ YourTake/Clean/Scenes/UserLogin/UserLoginViewController.swift
@@ -89,10 +89,6 @@ class UserLoginViewController: UIViewController,
     })
   }
   
-  @IBAction func forgotPasswordButtonPressed(_ sender: Any) {
-    router.navigateToForgotPasswordScene()
-  }
-  
   @IBAction func signupButtonPressed(_ sender: UIButton) {
     router.navigateToSignupScene()
   }
