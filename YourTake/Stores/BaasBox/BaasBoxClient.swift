aac75ca8524c4d776dbf3f394c91458c3076378a
johnbuonassisi <johnbuonassisi@gmail.com>
2017-08-02 06:35:17 -0700

    Update URL and appCode

--- YourTake/Stores/BaasBox/BaasBoxClient.swift
+++ YourTake/Stores/BaasBox/BaasBoxClient.swift
@@ -8,7 +8,7 @@
 
 class BaasBoxClient: BaClient {
     let client: BAAClient = {
-        BaasBox.setBaseURL("http://localhost:9000", appCode: "5965980156")
+        BaasBox.setBaseURL("http://192.168.0.12:9000", appCode: "1234567890")
         return BAAClient.shared()!
     }()
 
